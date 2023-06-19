b7665b588c63b48b7fbbc3059b49f2cb36ce056a
danish <drmughal03244@gmail.com>
2023-06-19 15:25:19 +0500

    adding Font family

--- app/src/main/java/com/wgroup/woooo_app/MainActivity.kt
+++ app/src/main/java/com/wgroup/woooo_app/MainActivity.kt
@@ -3,11 +3,14 @@ package com.wgroup.woooo_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.wgroup.woooo_app.woooo.theme.Woooo_androidTheme
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Woooo")
                 }
             }
         }
@@ -31,10 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+   Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+       Text(
+           text = "Hello $name!",
+           modifier = modifier
+       )
+   }
 }
 
 @Preview(showBackground = true)
