ee2c61028b30fe1dbcd6a7e15a0eb7a277e7b1cb
danish <drmughal03244@gmail.com>
2023-06-20 08:53:25 +0500

    adding custom alert dialogue And App bar in project

---
+++ app/src/main/java/com/wgroup/woooo_app/woooo/shared/components/CustomAlertDialogue.kt
@@ -0,0 +1,40 @@
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun ShowAlertDialog() {
+    val textBox = Color(0xff45ACDB)
+
+    val openDialog = remember { mutableStateOf(true) }
+
+    if (openDialog.value) {
+        AlertDialog(confirmButton = { Text(text = "") },
+            onDismissRequest = { openDialog.value = false },
+            containerColor = textBox,
+
+            text = {
+                Row(
+                    modifier = Modifier.padding(top = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CircularProgressIndicator()
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = "Please Wait.......")
+                }
+            })
+    }
+}
