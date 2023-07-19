5ea1adb61f7342efc7fd1459a213f007c4f964da
ehsan0097 <muhammd.ehsan@wgroup.tech>
2023-07-19 19:55:48 +0500

    Login Api Implemented

--- src/main/java/woooo_app/woooo/data/models/BaseModel.kt
+++ src/main/java/woooo_app/woooo/data/models/BaseModel.kt
@@ -3,8 +3,8 @@ package woooo_app.woooo.data.models
 import woooo_app.woooo.data.models.auth.SignUpData
 
 class BaseModel(
-    val success: Boolean? = null,
-    val message: String? = null,
-    val error: Any? = null,
-    val data: Any? = null
+    val Success: Boolean? = null,
+    val Message: String? = null,
+    val Error: Any? = null,
+    val Data: Any? = null
 )
--- src/main/java/woooo_app/woooo/shared/base/BaseRepository.kt
+++ src/main/java/woooo_app/woooo/shared/base/BaseRepository.kt
@@ -1,5 +1,7 @@
 package woooo_app.woooo.shared.base
+
 import android.util.Log
+import com.google.gson.Gson
 import com.google.gson.JsonParser
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -7,88 +9,59 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import okio.BufferedSource
-import org.json.JSONArray
-import org.json.JSONObject
 import retrofit2.Response
 import woooo_app.woooo.data.models.BaseModel
 
 abstract class BaseRepository {
 
     suspend fun <T> safeApiCall(
-        dispatcher:CoroutineDispatcher = Dispatchers.IO,
-        apiCall:suspend ()-> Response<T>
-    ):Flow<APIResult<T>> = flow {
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        apiCall: suspend () -> Response<T>
+    ): Flow<APIResult<T>> = flow {
         emit(APIResult.Loading)
         val response = apiCall()
+//        Log.d("SafeAPICall StatusCode", response.code().toString())
+//        Log.d("SafeAPICall Message", response.message())
+//        Log.d("SafeAPICall Successful", response.isSuccessful.toString())
+//        Log.d("SafeAPICall headers", response.headers().toString())
+//        Log.d("SafeAPICall raw", response.raw().toString())
+//        Log.d("SafeAPICall Body", response.body().toString())
+//        Log.d("SafeAPICall Error", response.errorBody()?.source().toString())
+//        Log.d("SafeAPICall Error ", response.errorBody()?.charStream().toString())
+////        Log.d("SafeAPICall Error", response.errorBody().source().readUtf8Line().toString())
+//
+//
+////        Log.d("SafeAPICall fromJson",json.toString())
+////        val jsonObj = JSONObject(bufferUTF)
+////        val map = jsonObj.toMap()
 
-        Log.d("SafeAPICall StatusCode",response.code().toString())
-        Log.d("SafeAPICall Message",response.message())
-        Log.d("SafeAPICall Successful",response.isSuccessful.toString())
-        Log.d("SafeAPICall headers",response.headers().toString())
-        Log.d("SafeAPICall raw",response.raw().toString())
-        Log.d("SafeAPICall Body",response.body().toString())
-        Log.d("SafeAPICall Error", response.errorBody()?.source().toString())
-        Log.d("SafeAPICall Error ", response.errorBody()?.charStream().toString())
-//        Log.d("SafeAPICall Error", response.errorBody().source().readUtf8Line().toString())
-
-
-        val bufferUTF= response.errorBody()!!.source().readUtf8Line().toString();
-        val parser = JsonParser()
-        val json = parser
-//        val jsonObj = JSONObject(bufferUTF)
-//        val map = jsonObj.toMap()
-
-
-//        val data= Gson().fromJson(JSONObject(), Map::class.java)
-//        val data= gson.fromJson(errorBody, Map::class.java)
-//        Log.d("SafeAPICall fromJson",map.toString())
-
-
-        if (response.isSuccessful){
+        if (response.isSuccessful) {
             val data = response.body()
-            if(data != null){
+            if (data != null) {
                 emit(APIResult.Success(data))
-            }else{
+            } else {
                 val error = response.errorBody()
-                if(error != null){
-//                    emit(APIResult.Failure(IOException(error.toString())))
-                }else{
-//                    emit(APIResult.Failure(IOException("something went wrong")))
+                if (error != null) {
+
+                    emit(APIResult.Failure(parseErrorBody(response)))
+                } else {
+                    emit(APIResult.Failure(BaseModel(Message = "Something went wrong")))
                 }
             }
-        }else{
-            emit(APIResult.Failure(BaseModel()))
+        } else {
+            emit(APIResult.Failure(parseErrorBody(response)))
         }
-    }.catch { e->
+    }.catch { e ->
         e.printStackTrace()
-//        emit(APIResult.Failure(Exception(e)))
+        emit(APIResult.Failure(BaseModel(Message = e.message)))
     }.flowOn(dispatcher)
 
 }
 
-suspend fun parseErrorBody(source: BufferedSource): String {
-    val stringBuilder = StringBuilder()
-    while (true) {
-        val data = source.readUtf8Line()
-        if (data!=null && data.isEmpty()) {
-            break
-        }
-        stringBuilder.append(data)
-    }
-    return stringBuilder.toString()
-}
 
-fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
-    when (val value = this[it])
-    {
-        is JSONArray ->
-        {
-            val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
-            JSONObject(map).toMap().values.toList()
-        }
-        is JSONObject -> value.toMap()
-        JSONObject.NULL -> null
-        else            -> value
-    }
+private fun <T> parseErrorBody(response: Response<T>): BaseModel {
+    val bufferUTF = response.errorBody()?.source()?.readUtf8Line().toString();
+    val parser = JsonParser()
+    val json = parser.parse(bufferUTF)
+    return Gson().fromJson(json, BaseModel::class.java);
 }
